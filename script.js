c6ceedd093b52321cfef4a9aabcbd79ff6eef7e6
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-02-06 00:03:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function mapAString(str = prompt("enter a string value")) {
+    let mapObj = {
+
+    };
+    
+    for(let i = 0; i < str.length; i++) {
+        let ch = str.charAt(i);
+        //if we encountered this character for the first time, declare new array
+        if(mapObj[ch] === undefined) {
+            mapObj[ch] = [i];
+        }
+        else{
+            // otherwise update existing array
+            mapObj[ch].push(i);
+        }
+    }
+    return mapObj;
+}
